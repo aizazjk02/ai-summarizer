a5ef7ff00b97d3f1dab9813b9310cd3314a1d553
aizaz <aizazkotwal2204@outlook.com>
2023-09-16 22:41:35 +0530

    feat: add hero component.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,21 @@
+import {logo} from "../assets"
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+        <nav className="flex justify-between items-center w-full mb-10 pt-3">
+            <img src={logo} alt="logo" className="w-28 object-contain"/>
+            <button type="button" className="black_btn" onClick={() => {}}>Github</button>
+        </nav>
+        <h1 className="head_text">
+            Summarize article with <br className="md:hidden"/>
+            <span className="orange_gradient">OpenAI GPT-4</span>
+        </h1>
+        <h2 className="desc">
+            Simplify your reading with Summarize, an open-source article summarizer that transforms lenghty articles into clear and concise summaries.
+        </h2>
+    </header>
+  ) 
+  
 }
 
 export default Hero;
