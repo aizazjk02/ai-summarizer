2a764aebf2294875357c381b7fa50b8019c57774
aizaz <aizazkotwal2204@outlook.com>
2023-09-17 00:29:03 +0530

    feat: add github url.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
     <header className="w-full flex justify-center items-center flex-col">
         <nav className="flex justify-between items-center w-full mb-10 pt-3">
             <img src={logo} alt="logo" className="w-28 object-contain"/>
-            <button type="button" className="black_btn" onClick={() => {}}>Github</button>
+            <button type="button" className="black_btn" onClick={() => {window.open("https://github.com/aizazjk02/ai-summarizer")}}>Github</button>
         </nav>
         <h1 className="head_text">
             Summarize article with <br className="md:hidden"/>
